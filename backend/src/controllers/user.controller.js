7465fee0c0ae72f0eff12c0ba466adff1df15836
Tochi-Ezeani <ezeanibarry1@gmail.com>
2026-04-13 10:30:49 +0100

    updating app.js for routes

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,37 @@
+import { User } from "../models/user.model.js";
+
+const reqisterUser = async (req, res) => {
+  try {
+    const { username, password, email } = req.body;
+
+    // Basic Validation
+    if (!username || !password || !email) {
+      return res.status(400).json({ message: "All fields are important!" });
+    }
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ email: email.toLowerCase() });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists!" });
+    }
+
+    // Create user
+    const user = await User.create({
+      username,
+      password,
+      email: email.toLowerCase(),
+      loggedIn: false,
+    });
+
+    res.status(201).json({
+      message: "User registered",
+      user: { id: user._id, email: user.email, username: user.username },
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+export { reqisterUser };
