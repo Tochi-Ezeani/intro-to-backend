eb221f1343c14240b221c86bc3b138fc5a3cc4fc
Tochi-Ezeani <ezeanibarry1@gmail.com>
2026-04-20 12:39:19 +0100

    feat: creating CRUD apis

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -68,4 +68,18 @@ const loginUser = async (req, res) => {
   }
 };
 
-export { registerUser, loginUser };
+const logoutUser = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email: email.toLowerCase() });
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.status(200).json({ message: "Logout successful" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error", error });
+  }
+};
+
+export { registerUser, loginUser, logoutUser };
